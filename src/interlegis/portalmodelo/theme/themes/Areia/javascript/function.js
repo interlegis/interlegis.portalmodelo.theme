562a58a529412f093f18ba45877fefa089bfce92
Caio Viotti <caio.viotti@gmail.com>
2014-08-26 18:32:12 -0300

    Improved JS code for collapsible menu on mobile.

--- src/interlegis/portalmodelo/theme/themes/Areia/javascript/function.js
+++ src/interlegis/portalmodelo/theme/themes/Areia/javascript/function.js
@@ -1,13 +1,20 @@
-jQuery(document).ready(function() {
+$(document).ready(function() {
+    var $portletItem = $('.portletNavigationTree .portletItem'),
+        $portletHeader = $('.portletNavigationTree .portletHeader');
 
-    size = jQuery( document ).width();
+    $(window).resize(function() {
+        if ($(window).width() < 749) {
+            $portletHeader.unbind();
+            $portletItem.hide();
+            $portletHeader.click(function(e) {
+                e.preventDefault();
+                $(this).next().slideToggle();
+            });
+        } else {
+            $portletItem.show();
+            $portletHeader.unbind();
 
-    if ( size < 749 ) {
-        jQuery(".portletNavigationTree .portletItem").addClass("hiddenStructure");
-        jQuery('.portletNavigationTree .portletHeader').click(function(e) {
-            e.preventDefault();
-            jQuery(".portletNavigationTree .portletItem").addClass("hiddenStructure");
-            jQuery(this).next().toggleClass("hiddenStructure");
-        });
-    }
+        }
+    }).resize();
+
 });
--- src/interlegis/portalmodelo/theme/themes/Gelo/javascript/function.js
+++ src/interlegis/portalmodelo/theme/themes/Gelo/javascript/function.js
@@ -1,18 +1,20 @@
-jQuery(document).ready(function(){
-  
-size = jQuery( document ).width();
+$(document).ready(function() {
+    var $portletItem = $('.portletNavigationTree .portletItem'),
+        $portletHeader = $('.portletNavigationTree .portletHeader');
 
-if ( size < 749 ){
+    $(window).resize(function() {
+        if ($(window).width() < 749) {
+            $portletHeader.unbind();
+            $portletItem.hide();
+            $portletHeader.click(function(e) {
+                e.preventDefault();
+                $(this).next().slideToggle();
+            });
+        } else {
+            $portletItem.show();
+            $portletHeader.unbind();
 
-jQuery(".portletNavigationTree .portletItem").addClass("hiddenStructure");
-jQuery('.portletNavigationTree .portletHeader').click(function(e){
-e.preventDefault();
-jQuery(".portletNavigationTree .portletItem").addClass("hiddenStructure");
-jQuery(this).next().toggleClass("hiddenStructure");
+        }
+    }).resize();
+
 });
-}
-
-});
-
-
-
