87ab919fe5c4fa5b5cf426458b1ca4145edaac39
Jean Ferri <jeanferri@interlegis.leg.br>
2015-08-26 15:22:52 -0300

    Back to development: 1.0rc5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0rc4'
+version = '1.0rc5.dev0'
 description = 'Pacote de temas do Portal Modelo'
 long_description = (
     open('README.rst').read() + '\n' +
