a0f1f1a0c1c5a3a315cd82e79553d1a8d35045ee
Caio Viotti <caio.viotti@gmail.com>
2015-10-01 16:32:32 -0300

    Fix width of tile-parlamentar list-items

--- src/interlegis/portalmodelo/theme/static/scripts/base.js
+++ src/interlegis/portalmodelo/theme/static/scripts/base.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
-
+    //Set width for parliamentarian list item based on its image size.
+    var parlamentarWidth = parseInt($('.tile-parlamentares img').attr('width'));
+    $('.tile-parlamentares .list-item').css('width', parlamentarWidth/2 + parlamentarWidth);
 });
