ff99e3dfa1b8e7d4205f445d3af06020faefc0d4
Rafahela <rafahela@gmail.com>
2018-03-22 09:54:46 -0300

    Add players for radio and video.

---
+++ src/interlegis/portalmodelo/theme/browser/videoaovivo.py
@@ -0,0 +1,7 @@
+from Products.Five import BrowserView
+
+class VideoAoVivoView(BrowserView):
+
+    def __init__(self, context, request):
+        super(VideoAoVivoView, self).__init__(context, request)
+
