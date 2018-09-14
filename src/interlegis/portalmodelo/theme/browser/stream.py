2f2601d9cd56c8ec25e7bd9bd957ba524e0740bf
rafahela <rafahela@gmail.com>
2018-09-14 17:56:02 -0300

    Create Stream type and portlet.

---
+++ src/interlegis/portalmodelo/theme/browser/stream.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from five import grok
+from interlegis.portalmodelo.theme.interfaces import IStream
+from Products.Five import BrowserView
+
+class StreamView(BrowserView):
+
+    def option(self):
+        return self.option()
+
+    def link(self):
+        return self.link()
--- src/interlegis/portalmodelo/theme/browser/videoaovivo.py
+++
@@ -1,8 +0,0 @@
-from Products.Five import BrowserView
-
-
-class VideoAoVivoView(BrowserView):
-
-    def __init__(self, context, request):
-        super(VideoAoVivoView, self).__init__(context, request)
-
