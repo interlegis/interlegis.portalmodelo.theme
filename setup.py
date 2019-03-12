98f56595d710c1105ae8133b6a8c9f2438776501
rafahela <rafahela@gmail.com>
2019-03-12 15:51:56 -0300

    Preparing release 1.0rc13

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0rc13.dev0'
+version = '1.0rc13'
 description = 'Pacote de temas do Portal Modelo'
 long_description = (
     open('README.rst').read() + '\n' +
